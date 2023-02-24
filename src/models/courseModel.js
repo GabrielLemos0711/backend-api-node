0675ba7bc1084b17a420d1cc0f0f3abbaeb9e74c
GabrielLemos0711 <gabrieldelemos157@icloud.com>
2023-02-24 17:34:36 -0300

    add create course and json parser

--- src/models/courseModel.js
+++ src/models/courseModel.js
@@ -1,26 +1,30 @@
-const mysql = require('mysql')
+const con = require('../db/dbConnection')
+
 const courseModel = {}
 
-
 courseModel.listAllCourses = (callback) => {
-  const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "apinode2"
-  })
   const sql = "SELECT * FROM cursos;"
-  con.query(sql, (err, results) => {
+  con.query(sql, (err, result) => {
     if (err) {
       callback(err, null)
     } else {
-      callback(null, results)
+      callback(null, result)
     }
   })
 }
 
-courseModel.createCourse = (req, res) => {
-  res.json({ message: "Entrou na rota /course com POST!" })
+courseModel.createCourse = (course, callback) => {
+  const { curso, cargahoraria } = course
+  const sql = 'INSERT INTO cursos (nome, cargahoraria) VALUES (?, ?);'
+  const values = [curso, cargahoraria]
+
+  con.query(sql, values, (err, result) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, result)
+    }
+  })
 }
 
 module.exports = courseModel
